53194aa62c1dab2052960419829ad5e9bb60af42
Jannick Oste <oste.jannick@hotmail.com>
2026-02-23 20:43:36 +0100

    Missing items added to network scanner container

--- backend/src/lib/lan-scanner/container.module.ts
+++ backend/src/lib/lan-scanner/container.module.ts
@@ -5,12 +5,18 @@ import HostInformationResolver from "./src/resolver/HostInformationResolver";
 import HostProbeService from "./src/service/HostProbeService";
 import PortProbeService from "./src/service/PortProbeService";
 import PortInformationResolver from "./src/resolver/PortInformationResolver";
+import NetworkInformationManager from "./src/manager/networkInformationManager";
+import NetworkProbeService from "./src/service/NetworkProbeService";
 
 const lanScannerContainerModule = new ContainerModule((bind) => {
     bind(HostInformationRepository).toSelf().inSingletonScope();
     bind(HostInformationService).toSelf().inSingletonScope();
+    bind(NetworkInformationManager).toSelf().inSingletonScope();
+
+    bind(NetworkProbeService).toSelf().inSingletonScope();
     bind(HostProbeService).toSelf().inSingletonScope();
     bind(PortProbeService).toSelf().inSingletonScope();
+    
     bind(PortInformationResolver).toSelf().inSingletonScope();
     bind(HostInformationResolver).toSelf().inSingletonScope();
 })
